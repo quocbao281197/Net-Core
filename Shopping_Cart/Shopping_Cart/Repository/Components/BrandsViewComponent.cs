85e91f928051749125abbb9faff696adbdf3520b
Billy Nguyen <quocbao281197@gmail.com>
2024-11-05 21:07:39 +0700

    #7-Move-View-To-ViewComponent-RegisterInViewImports-Add-Shared-Components

---
+++ Shopping_Cart/Shopping_Cart/Repository/Components/BrandsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping_Cart.Repository.Components
+{
+	public class BrandsViewComponent : ViewComponent
+	{
+		public readonly DataContext _dataContext;
+        public BrandsViewComponent(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Brands.ToListAsync());
+    }
+}
---
+++ Shopping_Cart/Shopping_Cart/Repository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shopping_Cart.Repository.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+
+        public CategoriesViewComponent(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Categories.ToListAsync());
+    }
+}
